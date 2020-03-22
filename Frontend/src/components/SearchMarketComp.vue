6cb8f9486832993a3c5a3883ae4e26333b1200c8
Paku580 <pascal.kuhlmann2@web.de>
2020-03-22 16:26:24 +0100

    another commit

--- Frontend/src/components/SearchMarketComp.vue
+++ Frontend/src/components/SearchMarketComp.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <headline-comp v-bind:title="headlineTitle" v-bind:text="headlineText"></headline-comp>
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="12" sm="6">
+                    <v-text-field label="PLZ, Ort" outlined>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <v-text-field label="Märkte" outlined>
+                    </v-text-field>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
@@ -10,8 +22,8 @@
         name: "SearchMarketComp",
         components: {HeadlineComp},
         data: () => ({
-            headlineTitle: "Das hier ist der Titel",
-            headlineText: "Das ist dann der ganze TExt dazu"
+            headlineTitle: "Ort und Markt eingeben",
+            headlineText: "Bitte trage eine PLZ und einen Ort ein. Wähle einen Markt aus und klicke dann auf \"Weiter\"."
         })
     }
 </script>
