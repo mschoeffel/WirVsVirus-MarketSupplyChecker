0e84f2ff774b9bc783e114031e1526d482ba3068
mschoeffel <michael.schoeffel@t-online.de>
2020-03-22 10:52:32 +0100

    Added Views + Routes

---
+++ Frontend/src/views/AddInfoQuantity.vue
@@ -0,0 +1,21 @@
+<template>
+    <layout-default>
+        <div class="home">
+            <HelloWorld/>
+        </div>
+    </layout-default>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+    import LayoutDefault from "../layouts/LayoutDefault";
+
+    export default {
+        name: 'AddInfoQuantity',
+        components: {
+            LayoutDefault,
+            HelloWorld
+        }
+    }
+</script>
