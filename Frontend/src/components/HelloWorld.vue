3950c67b6625de52de3d6db5125556f219ed4990
Paku580 <pascal.kuhlmann2@web.de>
2020-03-22 10:45:05 +0100

    first steps

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -12,16 +12,12 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Kurze Beschreibung was der Market Supply Checker macht und wie die Personen helfen
+          <br>hönnen bzw. wie es funktioniert
         </p>
       </v-col>
 
@@ -29,62 +25,8 @@
         class="mb-5"
         cols="12"
       >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+          <v-btn round color="primary" dark>Rounded Button</v-btn>
         </v-row>
       </v-col>
     </v-row>
--- Frontend/src/layouts/LayoutDefault.vue
+++ Frontend/src/layouts/LayoutDefault.vue
@@ -85,36 +85,10 @@
                     style="width: 300px"
                     class="ml-0 pl-4"
             >
-                <span class="hidden-sm-and-down">Google Contacts</span>
+                <span class="hidden-sm-and-down">Waren Radar</span>
             </v-toolbar-title>
-            <v-text-field
-                    flat
-                    solo-inverted
-                    hide-details
-                    prepend-inner-icon="mdi-magnify"
-                    label="Search"
-                    class="hidden-sm-and-down"
-            />
             <v-spacer />
-            <v-btn icon>
-                <v-icon>mdi-apps</v-icon>
-            </v-btn>
-            <v-btn icon>
-                <v-icon>mdi-bell</v-icon>
-            </v-btn>
-            <v-btn
-                    icon
-                    large
-            >
-                <v-avatar
-                        size="32px"
-                        item
-                >
-                    <v-img
-                            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-                            alt="Vuetify"
-                    /></v-avatar>
-            </v-btn>
+            <a href="/" class="v-btn v-btn--flat theme--dark">So Geht's</a>
         </v-app-bar>
         <v-content>
             <slot />
@@ -134,36 +108,11 @@
             dialog: false,
             drawer: null,
             items: [
-                { icon: 'mdi-contacts', text: 'Contacts' },
-                { icon: 'mdi-history', text: 'Frequently contacted' },
-                { icon: 'mdi-content-copy', text: 'Duplicates' },
-                {
-                    icon: 'mdi-chevron-up',
-                    'icon-alt': 'mdi-chevron-down',
-                    text: 'Labels',
-                    model: true,
-                    children: [
-                        { icon: 'mdi-plus', text: 'Create label' },
-                    ],
-                },
-                {
-                    icon: 'mdi-chevron-up',
-                    'icon-alt': 'mdi-chevron-down',
-                    text: 'More',
-                    model: false,
-                    children: [
-                        { text: 'Import' },
-                        { text: 'Export' },
-                        { text: 'Print' },
-                        { text: 'Undo changes' },
-                        { text: 'Other contacts' },
-                    ],
-                },
-                { icon: 'mdi-settings', text: 'Settings' },
-                { icon: 'mdi-message', text: 'Send feedback' },
-                { icon: 'mdi-help-circle', text: 'Help' },
-                { icon: 'mdi-cellphone-link', text: 'App downloads' },
-                { icon: 'mdi-keyboard', text: 'Go to the old version' },
+                { icon: 'mdi-contacts', text: 'Produkt suchen' },
+                { icon: 'mdi-history', text: 'Produkt eintragen' },
+                { icon: 'mdi-content-copy', text: 'So geht\'s' },
+                { icon: 'mdi-content-copy', text: '#WirVsVirus' },
+                { icon: 'mdi-keyboard', text: 'Github' }
             ],
         }),
         components: {
